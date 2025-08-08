eda59ae0401b851f0189125b7f4d4df6db50558c
asyounzz <youn.bllb@gmail.com>
2025-08-08 23:35:31 +0000

    Port

--- my-bot/index.js
+++ my-bot/index.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 const fs = require('node:fs');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = 4000;
 
 app.get('/', (req, res) => res.send('BombParty Bot is running!'));
 app.listen(port, () => console.log(`Web server listening on port ${port}`));
